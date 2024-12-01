6b122c1bb0645e3cebaad216b538d4645125a3ff
iwanhae <iwanhae@gmail.com>
2024-12-01 22:09:37 +0900

    mockup

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="background flex flex-col px-2">
+	<header class="flex flex-col w-full max-w-3xl gap-7 mx-auto mt-32 lg:mt-32 xl:mt-56 mb-14">
+		<div class="mx-auto text-4xl font-bold">
+			<h1>영한사전 / 한영사전</h1>
+		</div>
+		<div class="w-full max-w-3xl min-w-[200px]">
+			<input
+				class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-2xl border border-slate-200 rounded-full px-7 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+				placeholder="Type here..."
+			/>
+		</div>
+	</header>
+	<main class="flex flex-col mx-auto w-full">
+		<section class="mx-auto w-full max-w-3xl min-w-[200px]">
+			<div class="flex gap-3">
+				<h3 class="text-2xl">CORE</h3>
+				<p class="text-base mt-auto">명사</p>
+			</div>
+			<ul class="list-disc list-inside">
+				<li>
+					an organization founded by James Leonard Farmer in 1942 to work for racial equality.
+				</li>
+			</ul>
+			<hr class="h-px my-8 bg-gray-300 border-0" />
+		</section>
+
+		<section class="mx-auto w-full max-w-3xl min-w-[200px]">
+			<div class="flex gap-3">
+				<h3 class="text-2xl">core</h3>
+				<p class="text-base mt-auto">명사</p>
+			</div>
+
+			<div class="flex flex-col gap-3">
+				<div>
+					[<i class="text-gray-500"> core, core group, nucleus </i>]
+					<p>a small group of indispensable persons or things</p>
+				</div>
+				<div>
+					[<i class="text-gray-500"> core </i>]
+					<p>the center of an object</p>
+				</div>
+				<div>
+					[<i class="text-gray-500"> core </i>]
+					<p>the central part of the Earth</p>
+				</div>
+				<div>
+					<div class="text-gray-500 overflow-x-scroll">
+						[ center, centre, core, essence, gist, heart, heart and soul, inwardness, kernel,
+						marrow, meat, nitty-gritty, nub, pith, substance, sum]
+					</div>
+
+					<p>the choicest or most essential or most vital part of some idea or experience</p>
+				</div>
+				<div>
+					[<i class="text-gray-500"> core </i>]
+					<p>a cylindrical sample of soil or rock obtained with a hollow drill</p>
+				</div>
+			</div>
+			<hr class="h-px my-8 bg-gray-300 border-0" />
+		</section>
+	</main>
+</div>
+
+<style>
+	.background {
+		background: #f2f4f7;
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
